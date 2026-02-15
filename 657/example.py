1a6f9a0c6d2e74814de4f975bf3537fbf5d46a79
sanjay rao <sanjayraops17@gmail.com>
2026-02-15 20:09:55 +0530

    Robot Position

---
+++ 657/example.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def judgeCircle(self, moves):
+        x = 0
+        y = 0
+        for move in moves:
+            if move == "U":
+                y += 1
+            elif move == "D":
+                y -= 1
+            elif move == "R":
+                x += 1
+            elif move == "L":
+                x -= 1
+        if x == 0 and y == 0 :
+            return True
+        else: 
+            return False
+        
